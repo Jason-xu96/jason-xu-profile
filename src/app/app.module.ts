73171b39dfdd1cc22c103972ac2de742e1dd7e86
Jason Xu <jason-xu1996@hotmail.com>
2020-07-04 13:46:04 -0500

    Inital Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProjectCarouselComponent } from './components/project-carousel/project-carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    NavBarComponent,
+    ProjectCarouselComponent
   ],
   imports: [
     BrowserModule
